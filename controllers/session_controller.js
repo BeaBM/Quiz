8ebbc2ff0461a8d80c4e25b76975ec2b6fa17b60
jonathanyrula <jonathanyrula@gmail.com>
2015-05-15 18:23:05 +0200

    Autorizacion

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+// MW de autorización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next){
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+
 // Get /login   -- Formulario de login
 exports.new = function(req, res) {
     var errors = req.session.errors || {};
@@ -5,6 +14,8 @@ exports.new = function(req, res) {
 
     res.render('sessions/new', {errors: errors});
 };
+
+
 
 // POST /login   -- Crear la sesion si usuario se autentica
 exports.create = function(req, res) {
