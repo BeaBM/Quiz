fcf80526d1d4c91282cf3025510b79706d38ea33
jonathanyrula <jonathanyrula@gmail.com>
2015-05-18 17:01:25 +0200

    Roles y permisos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,6 +19,19 @@ exports.answer = function(req, res) {
 };
 */
 
+exports.ownershipRequired = function(req,res,next){
+  var objQuizOwner = req.quiz.UserId;
+  var logUser = req.session.user.id;
+  var isAdmin = req.session.user.isAdmin;
+
+  if(isAdmin ||objQuizOwner === logUser){
+    next();
+  }else{
+    res.redirect('/');
+  }
+};
+
+
 //GET /quizes/:id
 exports.show = function(req, res){
 	  res.render('quizes/show', { quiz: req.quiz, errors: []});  
